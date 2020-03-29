91790e7a94258d86791cb23552f46346e5169026
rolex <drolewski@gmail.com>
2020-03-29 22:08:43 +0200

    Create simple localization list - view setup

---
+++ app/src/main/java/io/drolewski/storageroom/activities/LocalizationActivity.kt
@@ -0,0 +1,13 @@
+package io.drolewski.storageroom.activities
+
+import android.app.Activity
+import android.os.Bundle
+
+class LocalizationActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_localization)
+
+    }
+}
--- app/src/main/java/io/drolewski/storageroom/activities/MainActivity.kt
+++ app/src/main/java/io/drolewski/storageroom/activities/MainActivity.kt
@@ -1,6 +1,8 @@
-package io.drolewski.storageroom
+package io.drolewski.storageroom.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getLocalization(view: View) {
+        val intent = Intent(this, LocalizationActivity::class.java)
+        startActivity(intent)
     }
 }
